2694e164fa352fa5202ca881aa3c015463dff2ea
ghada el abed <ghadaelabed22@gmail.com>
2023-02-16 12:20:08 -0800

    puts function in a c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return(0); }
+
+/* main function that always prints 0 but prints the below sentence in the process */
+
+int main(void) 
+{
+       	puts("\"Programming is like building a multilingual puzzle"); 
+	return(0); 
+}

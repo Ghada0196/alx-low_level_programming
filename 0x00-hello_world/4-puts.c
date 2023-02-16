8e9d9747e131da8475d11d7a237dda6db12a7a90
ghada el abed <ghadaelabed22@gmail.com>
2023-02-16 12:25:46 -0800

    betty error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/* main function that always prints 0 but prints the below sentence in the process */
+/** 
+* main function
+* that always prints 0
+* but prints the below sentence in the process 
+*/
 
-int main (void) 
+int main(void) 
 {
-       	puts ("\"Programming is like building a multilingual puzzle"); 
+	puts ("\"Programming is like building a multilingual puzzle"); 
 	return (0); 
 }

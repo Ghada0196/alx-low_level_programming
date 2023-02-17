c9888b6c3515a4ebbe41fe695f443b0b3c424b11
ghada el abed <ghadaelabed22@gmail.com>
2023-02-17 14:29:33 -0800

    print alphabet in lowercase using putchar in  a for loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,16 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 
 	char alphabet;
 
-	for( alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-		putchar('\n');
 	}
+	putchar('\n');
+	
 	return (0);
 }

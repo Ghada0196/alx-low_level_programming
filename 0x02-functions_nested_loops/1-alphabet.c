6374e0703ae96b15f87daeaa93172416f64cc04c
ghada el abed <ghadaelabed22@gmail.com>
2023-02-22 15:23:48 -0800

    prototype put_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * desc: print all alphabet in lowercase
+ * always return 0(success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);	
+	_putchar('\n');
+	
+	return (0);
+}

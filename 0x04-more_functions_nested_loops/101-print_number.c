92c8518e8b04c194e4b69a44ca5cd11f904b9d3b
ghada el abed <ghadaelabed22@gmail.com>
2023-02-24 13:33:56 -0800

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,18 +10,18 @@
 void print_number(int n)
 {
 	int i = 1;
-	int r;
+	int r = n;
 	
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
+		r = r * -1;
 	}
 	if (n < 10)
 		_putchar(n + '0');
 	else
 	{
-		r = n;
 		while (r > 9)
 		{
 			i = i * 10;
@@ -33,5 +33,4 @@ void print_number(int n)
 			i = i / 10;
 		}
 	}
-}
-	
+}	

8854a255976102ff91aebeb751640d44992f5791
ghada el abed <ghadaelabed22@gmail.com>
2023-02-24 12:10:52 -0800

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check description
+ * @n: an input number
+ * description: draws a diagonal line
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\');
+		_putchar('\n');
+	}
+}

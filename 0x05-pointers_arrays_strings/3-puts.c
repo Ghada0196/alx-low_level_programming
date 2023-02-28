0fd074def6ba5822d7d9d043403d55d667e43dfa
ghada el abed <ghadaelabed22@gmail.com>
2023-02-27 16:54:17 -0800

    print a line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - check description
+ * @str: an input pointer
+ * description: prints a string, followed by a new line, to stdout
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}

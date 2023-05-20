69abbbb58dd03bed3afc04548182ad0151508d38
ghada <ghadaelabed22@gmail.com>
2023-05-20 13:51:19 -0700

    more tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use printf to print a sentence
+ *
+ * Return: always 0
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

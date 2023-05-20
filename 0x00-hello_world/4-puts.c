7ea2cdd1b083d0e44bbfded1015eaac33b950965
ghada <ghadaelabed22@gmail.com>
2023-05-20 13:35:20 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use puts function to print a string
+ *
+ * Return: always 0
+ *
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}

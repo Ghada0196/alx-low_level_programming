8774697b4aa13dfe979ddd8c0d68b9003d36672a
ghada <ghadaelabed22@gmail.com>
2023-05-23 14:06:43 -0700

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print 50 fibonacci numbers
+ *
+ * Return: always 0
+ *
+*/
+
+int main(void)
+{
+	int i, sum;
+	int a = 1;
+	int b = 2;
+
+	print("1, 2, ");
+
+	for (i = 0; i < 50)
+	{
+		sum = a + b;
+		printf("%d, ", sum);
+		a = b;
+		b = sum;
+	}
+
+	printf("\n");
+
+	return (0);
+}

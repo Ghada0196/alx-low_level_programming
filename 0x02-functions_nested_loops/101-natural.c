323c67370597cd9187d39aa10b7246a1d98008ce
ghada <ghadaelabed22@gmail.com>
2023-05-23 13:55:01 -0700

    fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - computes and prints the sum of
- * 	all the multiples of 3 or 5 below 1024
+ * all the multiples of 3 or 5 below 1024
  * Return: always 0
  *
 */
@@ -18,6 +18,6 @@ int main(void)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
-	
+
 	return (0);
 }

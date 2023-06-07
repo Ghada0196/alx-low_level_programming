2bccfea264893f2d2c7d9dfb736293bf200b9b7b
ghada <ghadaelabed22@gmail.com>
2023-06-07 13:14:54 -0700

    first task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints a strig
+ * @s: the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		_putchar(s[i]);
+
+	return (0);
+}

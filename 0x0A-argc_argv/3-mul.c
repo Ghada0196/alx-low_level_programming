6a56089a4a9b8c3f5dd62cc380303e3ab7ddc4ab
ghada <ghadaelabed22@gmail.com>
2023-06-09 16:34:02 -0700

    task 3, the atoi bit was unexpected

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply 2 numbers
+ * @argc: number of arguments
+ * @argv: arguments containing the numbers
+ *
+ * Return: the result
+ */
+
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+
+		return (0);
+	}
+}

cdd24b7cdffeaa3bc2ed4d4459244fbd5cf6a96a
ghada <ghadaelabed22@gmail.com>
2023-06-20 15:38:03 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints the name of the file it was compiled from,
+ * main - prints the name of the file it was compiled from
  * followed by a new line.
  *
  * Return: always 0

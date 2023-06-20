d1c87630cefa19b8cf698967a3bec39ca2c5a109
ghada <ghadaelabed22@gmail.com>
2023-06-20 15:51:31 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,7 +2,9 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
- * SIZE: an abbreviation for the token 1024
+ * file: 0-object_like_macro.h
+ * description: defines a macro named SIZE
+ * as an abbreviation for the token 1024
  */
 
 #define SIZE (1024)

f4c03e31722f043321209efba1c78727474a8705
ghada <ghadaelabed22@gmail.com>
2023-06-21 14:27:17 -0700

    added NULL condition

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (array == NULL || size < 0 || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)

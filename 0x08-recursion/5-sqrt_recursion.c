7a902bcacdd4a4ae26fbf867a3690b94edd50bad
ghada <ghadaelabed22@gmail.com>
2023-07-03 12:53:46 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * find_root - find rout of number n
+ * @n: the number
+ * @root: the root
+ * Return: the coorect root if found
+ */
+
+int find_root(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_root(num, root + 1));
+}
+
+/**
  *  _sqrt_recursion - returns the natural square root of a number
  *  @n: the number
  *
@@ -9,13 +27,10 @@
 
 int _sqrt_recursion(int n)
 {
-	int num = n;
+	int root = 0;
 
 	if (n < 0)
 		return (-1);
 
-	if (n * n != num)
-		_sqrt_recursion(n - 1);
-
-	return (n);
+	return (find_root(n, root));
 }

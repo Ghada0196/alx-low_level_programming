f17628fdf098286c445c8794a12a0f06401b294a
ghada <ghadaelabed22@gmail.com>
2023-07-05 12:40:49 -0700

    fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,13 +2,16 @@
 
 /**
  * free_listint2 -  frees a listint_t list.
- * @h: pointer to the first node
+ * @head: pointer to the first node
  * return: nothing
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
+	if (!(*head))
+		return;
 
 	while (*head)
 	{
